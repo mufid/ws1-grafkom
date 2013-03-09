989868203c6bb80cb2adf42823bb1d7ee56d5c9c
Mufid Afif <mufid@idmail.or.id>
2013-03-09 08:49:02 +0700

    The real initial commit

--- WS1 - Glutpaint.cpp
+++ WS1 - Glutpaint.cpp
@@ -55,30 +55,26 @@ or moved */
 
 void myReshape(GLsizei w, GLsizei h)
 {
+    /* adjust clipping box */
 
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity(); 
+    glOrtho(0.0, (GLdouble)w, 0.0, (GLdouble)h, -1.0, 1.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity(); 
 
-	return;
-	/* adjust clipping box */
-	printf("%d\n", (int) w);
+    /* adjust viewport and  clear */
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity(); 
-	glOrtho(0.0, (GLdouble)w, 0.0, (GLdouble)h, -1.0, 1.0);
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity(); 
+    glViewport(0,0,w,h);
+    glClearColor (0.8, 0.8, 0.8, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    display();
+    glFlush();
 
-	/* adjust viewport and  clear */
+    /* set global size for use by drawing routine */
 
-	glViewport(0,0,500,100);
-	glClearColor (0.8, 0.8, 0.8, 1.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	display();
-	glFlush();
-
-	/* set global size for use by drawing routine */
-
-	ww = w;
-	wh = h; 
+    ww = w;
+    wh = h; 
 }
 
 void myinit(void)
